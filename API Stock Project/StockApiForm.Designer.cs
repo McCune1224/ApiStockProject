b4f48317ecc388208c0e4560ec1b0218d72c26f2
mccune1224 <alexmccune1224@gmail.com>
2020-10-19 00:00:09 -0700

    Importing Project, proof of concept and not much code to show...

---
+++ API Stock Project/StockApiForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace API_Stock_Project
+{
+    partial class StockApiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.outputInfoBox = new System.Windows.Forms.TextBox();
+            this.stockInfoCollectionButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputInfoBox
+            // 
+            this.outputInfoBox.Location = new System.Drawing.Point(55, 85);
+            this.outputInfoBox.Multiline = true;
+            this.outputInfoBox.Name = "outputInfoBox";
+            this.outputInfoBox.Size = new System.Drawing.Size(368, 234);
+            this.outputInfoBox.TabIndex = 0;
+            // 
+            // stockInfoCollectionButton
+            // 
+            this.stockInfoCollectionButton.Location = new System.Drawing.Point(471, 185);
+            this.stockInfoCollectionButton.Name = "stockInfoCollectionButton";
+            this.stockInfoCollectionButton.Size = new System.Drawing.Size(122, 23);
+            this.stockInfoCollectionButton.TabIndex = 1;
+            this.stockInfoCollectionButton.Text = "Get Stock Info";
+            this.stockInfoCollectionButton.UseVisualStyleBackColor = true;
+            this.stockInfoCollectionButton.Click += new System.EventHandler(this.stockInfoCollectionButton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(483, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(480, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Insert Stock Symbol:\r\n";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // StockApiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.stockInfoCollectionButton);
+            this.Controls.Add(this.outputInfoBox);
+            this.Name = "StockApiForm";
+            this.Text = "Stock Info API Form";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox outputInfoBox;
+        private System.Windows.Forms.Button stockInfoCollectionButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ API Stock Project/StockApiForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+//Newtsonsoft is required
+using Newtonsoft.Json;
+using System.Linq.Expressions;
+
+namespace API_Stock_Project
+{
+    public partial class StockApiForm : Form
+    {
+        public StockApiForm()
+        {
+            InitializeComponent();
+        }
+        //This is what is used to communicate with the web and get HTTP responses (aka the page we need).
+        HttpClient client = new HttpClient();
+
+        public void stockInfoCollectionButton_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            //Just some checking for any numbers, whitespaces, or special characters
+            string unwantedChars = @"\|!#$%&/()=?»«@£§€{}.-;'<>_,1234567890 ";
+            foreach (var ch in unwantedChars)
+            {
+                if (textBox2.Text.Contains(ch))
+                {
+                    errorProvider1.SetError(textBox2, "Please only use Characters! (No numbers, spaces, or special characters (!@#$...etc))");
+                }
+            }
+            if (textBox2.Text == "")
+            {
+                errorProvider1.SetError(textBox2, "No Symbol Inputted. Please input a Stock Symbol");
+
+            }
+            string symbol = textBox2.Text.Trim().ToUpper();
+            GetStockInfo("A7DCOEYGJAAD85T5", symbol);
+        }
+
+        //function that gets the stock info with the API key we have.
+        public async Task GetStockInfo(string apiKey = "demo", string symbol = "IBM")
+        {
+            string response = await client.GetStringAsync(
+                $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={apiKey}");
+
+            //I imported from NuGet packages Newtonsoft.Json which is SUPER helpful for converting info into JSON info we can access and manipulate
+            //The JsonConvert.DeserializeObject breaks the item down into a .Net object which we can work with similar to a key/value pair
+            dynamic stockJson = JsonConvert.DeserializeObject(response);
+
+            //so I wanted to use getters and setters for these, but due to the whitespaces in the JSON's key
+            //I didn't know how to declare the variables since they must match the name of the JSON object's key
+            string mostRecentDate = stockJson["Meta Data"]["3. Last Refreshed"];
+            string open = stockJson["Time Series (Daily)"][mostRecentDate]["1. open"];
+            string high = stockJson["Time Series (Daily)"][mostRecentDate]["2. high"];
+            string low = stockJson["Time Series (Daily)"][mostRecentDate]["3. low"];
+            string close = stockJson["Time Series (Daily)"][mostRecentDate]["4. close"];
+            string volume = stockJson["Time Series (Daily)"][mostRecentDate]["5. volume"];
+           
+            //print info to textbox
+            outputInfoBox.AppendLine($"Most Recent Date - {mostRecentDate}");
+            outputInfoBox.AppendLine($"Open Price - {open}");
+            outputInfoBox.AppendLine($"Close Price - {close}");
+
+            ;
+        }
+
+    }
+    //helper function for creating new line on multi-lined textbox
+    public static class WinFormsExtensions
+    {
+        public static void AppendLine(this TextBox source, string value)
+        {
+            if (source.Text.Length == 0)
+                source.Text = value;
+            else
+                source.AppendText("\r\n" + value);
+        }
+    }
+}
